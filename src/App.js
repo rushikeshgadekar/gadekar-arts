1b2cdb7e9cf522b106a827ea385921ea15084fac
Rushikesh Gadekar <rushikeshgadekar17399@gmail.com>
2020-12-14 00:26:20 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './assets/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello </h1>
+    <div className="app">
+      <div className='w-screen '>
+        <nav className="bg-gradient-to-r from-gray-400 via-gray-800 to-gray-400 hover:bg-gray-600  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch ">
+          <a href="#" className='bg-gray-900 hover:bg-gray-700 px-7 py-2 rounded-md text-lg font-sans-serif font-medium  text-center active-current nav-item is-active ' active-color='green'>HOME</a>
+          <a href="#" className='text-gray-100 hover:bg-gray-700 hover:text-white px-7 py-2 rounded-md text-lg font-sans-serif font-medium nav-item ' active-color='blue'>GALLERY</a>
+          <a href="#" className='text-gray-100 hover:bg-gray-700 hover:text-white px-1 py-2 rounded-md text-lg font-sans-serif font-medium nav-item ' active-color='red'>CONTACT US</a>
+          
+        </nav>
+      </div>
     </div>
   );
 }
