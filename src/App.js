714563d51c16c0040319dd67c58cce24adc04d9a
Rushikesh Gadekar <rushikeshgadekar17399@gmail.com>
2020-12-14 15:38:43 +0530

    added bg image

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import  './components/nav.css'
 
 function App() {
   return (
-    <div className="app">
-      <div className='w-screen container bg-gradient-to-r from-gray-400 via-gray-800 to-gray-400 hover:bg-gray-600  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch '>
+    <div className="app ">
+      <div className='w-screen  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch container'>
         {/* <nav className="bg-gradient-to-r from-gray-400 via-gray-800 to-gray-400 hover:bg-gray-600  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch "> */}
-          <a href="#" className='bg-gray-900 hover:bg-gray-700 px-7 py-2 rounded-md text-lg font-sans-serif font-medium  text-center nav-item '>HOME</a>
-          <a href="#" className='text-gray-100 hover:bg-gray-700 hover:text-white px-7 py-2 rounded-md text-lg font-sans-serif font-medium nav-item' >GALLERY</a>
-          <a href="#" className='text-gray-100 hover:bg-gray-700 hover:text-white px-1 py-2 rounded-md text-lg font-sans-serif font-medium nav-item '>CONTACT US</a>
-          
+          <a href="#" className=' px-8 py-4 hover:text-black rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME</a>
+          <a href="#" className='text-gray-100 hover:text-black px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GAllERY</a>
+          <a href="#" className='text-gray-100 hover:text-black px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</a>
         {/* </nav> */}
       </div>
     </div>
