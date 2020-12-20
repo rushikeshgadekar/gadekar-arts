9163c68e6d8bfa1d006a4635e7cb277c66f87aed
Rushikesh Gadekar <rushikeshgadekar17399@gmail.com>
2020-12-21 01:38:02 +0530

    retrying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {BrowserRouter, Switch, Route, NavLink, Redirect} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
 import './App.css';
 import './assets/main.css'
 import  './components/nav.css'
@@ -22,24 +22,22 @@ function App() {
       <BrowserRouter>
         
         { window.screen.width>480? <div className='w-screen  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch container'>
-          <NavLink to="Home" className=' px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME </NavLink>
-          <NavLink to="Gallery" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GALLERY</NavLink>
-          <NavLink to="Contact" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</NavLink>        
+          <NavLink to="home" className=' px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME </NavLink>
+          <NavLink to="gallery" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GALLERY</NavLink>
+          <NavLink to="contact" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</NavLink>        
           </div>: 
           <select value={input} onChange={select} >
-            <option value='Home'>HOME</option>
-            <option value='Gallery'>GALLERY</option>
-            <option value='Contact'>CONTACT US</option>
+            <option value='home'>HOME</option>
+            <option value='gallery'>GALLERY</option>
+            <option value='contact'>CONTACT US</option>
           </select>
           
           }
 
           <Switch>
-            <Route exact path='/' component={Home}>
-              <Redirect to = '/Home' />
-              </Route>
-            <Route exact path='/Gallery' component={Gallery} />
-            <Route exact path='/Contact' component={Contact} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/gallery' component={Gallery} />
+            <Route exact path='/contact' component={Contact} />
           </Switch>
       </BrowserRouter>
     </div>
