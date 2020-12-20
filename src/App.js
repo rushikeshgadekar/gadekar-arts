9ecc670490f709e84ee84f66948bd54522379b6c
Rushikesh Gadekar <rushikeshgadekar17399@gmail.com>
2020-12-20 23:10:09 +0530

    deploying4

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
 import './App.css';
 import './assets/main.css'
 import  './components/nav.css'
-import Gallery from './components/gallery'
-import Contact from './components/contact'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
 import Home from './components/Home'
 
 function App() {
@@ -22,9 +22,9 @@ function App() {
       <BrowserRouter>
         
         { window.screen.width>480? <div className='w-screen  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch container'>
-          <NavLink to="/home" className=' px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME </NavLink>
-          <NavLink to="/gallery" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GALLERY</NavLink>
-          <NavLink to="/contact" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</NavLink>        
+          <NavLink to="/gadekar-arts" className=' px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME </NavLink>
+          <NavLink to="/gadekar-arts/gallery" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GALLERY</NavLink>
+          <NavLink to="/gadekar-arts/contact" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</NavLink>        
           </div>: 
           <select value={input} onChange={select} >
             <option value='home'>HOME</option>
@@ -35,9 +35,9 @@ function App() {
           }
 
           <Switch>
-            <Route exact path='/gadekar-arts/home' component={Home}/>
-            <Route exact path='/gadekar-arts/gallery' component={Gallery} />
-            <Route exact path='/gadekar-arts/contact' component={Contact} />
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/gallery' component={Gallery} />
+            <Route exact path='/contact' component={Contact} />
           </Switch>
       </BrowserRouter>
     </div>
