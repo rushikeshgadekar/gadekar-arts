a754c67327a4eafb90ea675cbe9c5fe748ff9198
Ajinkya Gore <ajinkyagore2@gmail.com>
2020-12-21 19:36:10 +0530

    updated font

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
-import React, {useState} from 'react';
-import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
-import './App.css';
-import './assets/main.css'
-import  './components/nav.css'
-import Gallery from './components/Gallery'
-import Contact from './components/Contact'
-import Home from './components/Home'
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import "./App.css";
+import "./assets/main.css";
+import "./components/nav.css";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
-  const [input, setInput] = useState({value:'home'})
-  const select=(e)=>{
-     return(
-       
-        setInput(e.target.value ))
-    }
-
+  const [input, setInput] = useState({ value: "home" });
+  const select = (e) => {
+    return setInput(e.target.value);
+  };
 
   return (
     <div className="app ">
       {/* <div className='mask'></div> */}
       <BrowserRouter>
-        
-        { window.screen.width>480? <div className='w-screen  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch container'>
-          <NavLink to="/" className=' px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item '>HOME </NavLink>
-          <NavLink to="/gallery" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item' >GALLERY</NavLink>
-          <NavLink to="/contact" className='text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item '>CONTACT US</NavLink>        
-          </div>: 
-          <select value={input} onChange={select} >
-            <option value='home'>HOME</option>
-            <option value='gallery'>GALLERY</option>
-            <option value='contact'>CONTACT US</option>
+        {window.screen.width > 480 ? (
+          <div className="w-screen  p-7 text-white text-4xl flex space-x-10 justify-center items-center sm:items-stretch container">
+            <NavLink
+              exact
+              to="/"
+              className=" px-8 py-4 hover:text-pink-800 rounded-md text-xl font-sans-serif font-medium  text-center nav-item "
+              activeClassName="active"
+            >
+              HOME{" "}
+            </NavLink>
+            <NavLink
+              exact
+              to="/gallery"
+              className="text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item"
+              activeClassName="active"
+            >
+              GALLERY
+            </NavLink>
+            <NavLink
+              exact
+              to="/contact"
+              className="text-gray-100 hover:text-pink-800 px-8 py-4 rounded-md text-xl font-sans-serif font-medium nav-item "
+              activeClassName="active"
+            >
+              CONTACT US
+            </NavLink>
+          </div>
+        ) : (
+          <select value={input} onChange={select}>
+            <option value="home">HOME</option>
+            <option value="gallery">GALLERY</option>
+            <option value="contact">CONTACT US</option>
           </select>
-          
-          }
+        )}
 
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/gallery' component={Gallery} />
-            <Route exact path='/contact' component={Contact} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import '../App.css'
-import './home.css'
+import React from "react";
+import "../App.css";
+import "./home.css";
 import smoke from "../components/videos/smoke.mp4";
-function Home () {
-    return (
-        <div>
-            <section>
-                <video autoPlay playsInline loop muted src={smoke} />
-                <h1 >
-                    <span>G</span>  
-                    <span>A</span>
-                    <span>D</span>
-                    <span>E</span>
-                    <span>K</span>
-                    <span>A</span>
-                    <span>R</span>
-                    <span>~</span>
-                    <span>A</span>
-                    <span>R</span>
-                    <span>T</span>
-                    <span>S</span>
-                </h1>
-	        </section>
-        </div>
-        
-    );
+function Home() {
+  return (
+    <div>
+      <section>
+        <video autoPlay playsInline loop muted src={smoke} />
+        <h1 id="logo">
+          <span>G</span>
+          <span>A</span>
+          <span>D</span>
+          <span>E</span>
+          <span>K</span>
+          <span>A</span>
+          <span>R</span>
+          <span>~</span>
+          <span>A</span>
+          <span>R</span>
+          <span>T</span>
+          <span>S</span>
+        </h1>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
